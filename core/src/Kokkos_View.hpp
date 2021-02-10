c64fdaebfe274e5cc96190cbe06c2113107239f6
GitHub Ryanxiejh <GitHub 294732246@qq.com>
2021-02-10 16:05:08 +0800

    增加SwThread的View处理——普通构造函数和subview patch2

--- core/src/Kokkos_View.hpp
+++ core/src/Kokkos_View.hpp
@@ -1905,12 +1905,12 @@ class View : public ViewTraits<DataType, Properties...> {
 
 //Ryanxiejh 2021/2/10
 #if defined(KOKKOS_ENABLE_SWTHREAD)
-      data_ptr[curViewIndex] = this->data();
-        for( int i = 0; i < Rank ; i++ ){
-            data_stride[curViewIndex][i] = this->stride(i);
-            data_layout[i] = sw_LAYOUT_STRIDE;
-        }
-        curViewIndex += 1;
+//      data_ptr[curViewIndex] = this->data();
+//        for( int i = 0; i < Rank ; i++ ){
+//            data_stride[curViewIndex][i] = this->stride(i);
+//            data_layout[i] = sw_LAYOUT_STRIDE;
+//        }
+//        curViewIndex += 1;
         printf("subview created...\n");
 #endif
   }
