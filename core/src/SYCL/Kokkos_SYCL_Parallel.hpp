3b46608ae97805f562d0a2a493f77a56f85b5316
GitHub Ryanxiejh <GitHub 294732246@qq.com>
2021-02-21 21:15:03 +0800

    增加SYCL后端 parallel_for(MDRPolicy) test1

--- core/src/SYCL/Kokkos_SYCL_Parallel.hpp
+++ core/src/SYCL/Kokkos_SYCL_Parallel.hpp
@@ -167,7 +167,7 @@ class ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>, Kokkos::SYCL> {
         std::cout << "direct_launch !!!" << std::endl;
     }
     else{
-        //sycl_indirect_launch();
+        sycl_indirect_launch();
         std::cout << "sycl_indirect_launch !!!" << std::endl;
     }
   }
