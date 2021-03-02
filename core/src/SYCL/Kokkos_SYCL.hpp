3fe28692841e306768ad027c5b7f1a98b1586604
GitHub Ryanxiejh <GitHub 294732246@qq.com>
2021-03-02 10:12:57 +0800

    增加SYCL后端

--- core/src/SYCL/Kokkos_SYCL.hpp
+++ core/src/SYCL/Kokkos_SYCL.hpp
@@ -92,7 +92,7 @@ struct DeviceTypeTraits<SYCL> {
 } //namespace Kokkos
 
 #include <SYCL/Kokkos_SYCL_Instance.hpp>
-#include <SYCL/kokkos_SYCL_Atomic.hpp>
+#include <SYCL/Kokkos_SYCL_Atomic.hpp>
 #include <SYCL/Kokkos_SYCL_Team.hpp>
 #include <SYCL/Kokkos_SYCL_Parallel.hpp>
 #endif //#if defined(KOKKOS_ENABLE_SYCL)
